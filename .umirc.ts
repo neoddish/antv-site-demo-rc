4811938372c2147a921aae9a21cb8684befc39a8
neoddish <neoddish@outlook.com>
2021-11-09 11:25:54 +0800

    chore: fix umirc for gh-pages

--- .umirc.ts
+++ .umirc.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   outputPath: '.doc',
   exportStatic: {},
+  base: '/antv-site-demo-rc/',
+  publicPath: '/antv-site-demo-rc/',
+  hash: true,
   styles: [
     `
       .markdown table {
