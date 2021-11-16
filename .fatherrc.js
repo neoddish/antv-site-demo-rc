13c194b037db671a43dac4fc747f6943498bffab
neoddish <neoddish@outlook.com>
2021-11-16 13:56:22 +0800

    chore: umd pkg

--- .fatherrc.js
+++ .fatherrc.js
@@ -1,6 +1,7 @@
 export default {
   cjs: 'babel',
   esm: { type: 'babel', importLibToEs: true },
+  umd: { name: 'AntVSiteDemoRC', minFile: true, file: 'index' },
   preCommit: {
     eslint: true,
     prettier: true,
